8976d5e08f4dc0729b98ebdb21aebeb68c15b983
Sergey Zarochentsev <zsergey26@gmail.com>
2015-07-09 21:32:15 +0300

    MainActivity fix

--- app/src/main/java/com/udacity/zsergei/myappportfolio/MainActivity.java
+++ app/src/main/java/com/udacity/zsergei/myappportfolio/MainActivity.java
@@ -3,11 +3,14 @@ package com.udacity.zsergei.myappportfolio;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+    private static String TAG = "My App Portfolio ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,36 +19,70 @@ public class MainActivity extends Activity {
     }
 
 
-    public void spotifyStreamerLaunch(View view) {
+    public void spotifyStreamerLaunch() {
         showToast("This button will launch my spotify streamer app!");
     }
 
-    public void scoresAppLaunch(View view) {
+    public void scoresAppLaunch() {
         showToast("This button will launch my scores app!");
     }
 
-    public void libraryAppLaunch(View view) {
+    public void libraryAppLaunch() {
         showToast("This button will launch my library app!");
     }
 
-    public void buildItBiggerLaunch(View view) {
+    public void buildItBiggerLaunch() {
         showToast("This button will launch my 'build it bigger' app!");
     }
 
-    public void xyzReaderLaunch(View view) {
+    public void xyzReaderLaunch() {
         showToast("This button will launch my xyz reader app!");
     }
 
-    public void capstoneLaunch(View view) {
+    public void capstoneLaunch() {
         showToast("This button will launch my capstone app!");
     }
 
     private void showToast(String text)
     {
-        Context context = getApplicationContext();
+        Context context = MainActivity.this;
         int duration = Toast.LENGTH_SHORT;
 
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
+    }
+
+    public void onButtonClickListener(View view) {
+        switch(view.getId())
+        {
+            case R.id.spotifyStreamerButton :
+                spotifyStreamerLaunch();
+                break;
+
+            case R.id.scoresAppButton :
+                scoresAppLaunch();
+                break;
+
+            case R.id.libraryAppButton :
+                libraryAppLaunch();
+                break;
+
+            case R.id.buildItBiggerButton :
+                buildItBiggerLaunch();
+                break;
+
+            case R.id.xyzReaderButton :
+                xyzReaderLaunch();
+                break;
+
+            case R.id.capstoneButton :
+                capstoneLaunch();
+                break;
+
+            default:
+                Log.e(MainActivity.TAG, "ooops, something wrong!");
+                break;
+
+        }
     }
 }
