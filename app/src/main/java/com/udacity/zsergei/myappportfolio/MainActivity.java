7ba267bb3fc57b079c1ae6848812c71bff15a26e
Sergei Zarochentsev <zsergey26@gmail.com>
2015-07-09 14:38:44 +0300

    toast

--- app/src/main/java/com/udacity/zsergei/myappportfolio/MainActivity.java
+++ app/src/main/java/com/udacity/zsergei/myappportfolio/MainActivity.java
@@ -1,11 +1,13 @@
 package com.udacity.zsergei.myappportfolio;
 
 import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -18,20 +20,35 @@ public class MainActivity extends Activity {
 
 
     public void spotifyStreamerLaunch(View view) {
+        showToast("This button will launch my spotify streamer app!");
     }
 
     public void scoresAppLaunch(View view) {
+        showToast("This button will launch my scores app!");
     }
 
     public void libraryAppLaunch(View view) {
+        showToast("This button will launch my library app!");
     }
 
     public void buildItBiggerLaunch(View view) {
+        showToast("This button will launch my 'build it bigger' app!");
     }
 
     public void xyzReaderLaunch(View view) {
+        showToast("This button will launch my xyz reader app!");
     }
 
     public void capstoneLaunch(View view) {
+        showToast("This button will launch my capstone app!");
+    }
+
+    private void showToast(String text)
+    {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
